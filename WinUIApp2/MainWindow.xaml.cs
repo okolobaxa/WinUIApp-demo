b19d81dae0aa091cb5d8feaad14d19ba7c34e9a8
okolobaxa <okolobaxa@KHEYSTVER>
2021-12-30 17:18:57 +0100

    Add project files.

---
+++ WinUIApp2/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUIApp2
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+
+            var description = new System.Text.StringBuilder();
+            var process = System.Diagnostics.Process.GetCurrentProcess();
+            foreach (System.Diagnostics.ProcessModule module in process.Modules)
+            {
+                description.AppendLine(module.FileName);
+            }
+            cdTextBlock.Text = description.ToString();
+
+            cdTextBlock1.Text = $"IsRunningAsUwp: {new Helpers().IsRunningAsUwp()}";
+        }
+    }
+
+    public class Helpers
+    {
+        const long APPMODEL_ERROR_NO_PACKAGE = 15700L;
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        static extern int GetCurrentPackageFullName(ref int packageFullNameLength, StringBuilder packageFullName);
+
+        public bool IsRunningAsUwp()
+        {
+            if (IsWindows7OrLower)
+            {
+                return false;
+            }
+            else
+            {
+                int length = 0;
+                StringBuilder sb = new StringBuilder(0);
+                int result = GetCurrentPackageFullName(ref length, sb);
+
+                sb = new StringBuilder(length);
+                result = GetCurrentPackageFullName(ref length, sb);
+
+                return result != APPMODEL_ERROR_NO_PACKAGE;
+            }
+        }
+
+        private bool IsWindows7OrLower
+        {
+            get
+            {
+                int versionMajor = Environment.OSVersion.Version.Major;
+                int versionMinor = Environment.OSVersion.Version.Minor;
+                double version = versionMajor + (double)versionMinor / 10;
+                return version <= 6.1;
+            }
+        }
+    }
+}
